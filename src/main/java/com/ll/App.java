8fa9d41d67d53abf4700797d66066ea64e8e7eaf
hellonaeunkim <hellonaeunkim@gmail.com>
2024-11-22 17:53:47 +0900

    feat : 명언 반복 출력

---
+++ src/main/java/com/ll/App.java
@@ -0,0 +1,11 @@
+package com.ll;
+
+public class App {
+  public void run() {
+    System.out.println("== 명언 앱 ==");
+    while (true) {
+      System.out.print("명령 ) ");
+      String cmd = scanner.nextLine();
+    }
+  }
+}
