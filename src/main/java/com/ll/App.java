a0ac52b95c5a04ca7deec8885d12cfdea4c72c6b
hellonaeunkim <hellonaeunkim@gmail.com>
2024-11-25 23:33:44 +0900

    refactor : 레이어드 아키첵처로 변경

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import com.ll.domain.wiseSaying.entity.WiseSaying;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
---
+++ src/main/java/com/ll/domain/wiseSaying/controller/wiseSayingController.java
@@ -0,0 +1,4 @@
+package com.ll.domain.wiseSaying.controller;
+
+public class wiseSayingController {
+}
